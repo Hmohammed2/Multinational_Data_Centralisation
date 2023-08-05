9e983f2af50cb8ae6c996905aed7538212357202
Hamza <Hamza_mohammed15@hotmail.com>
2023-08-05 04:55:01 +0100

    modified methods

--- database_utils.py
+++ database_utils.py
@@ -1,10 +1,12 @@
 import yaml
 from sqlalchemy import create_engine, inspect
+from sqlalchemy.exc import SQLAlchemyError
 import pandas as pd
 
 class DatabaseConnector:
 
     def __init__(self) -> None:
+        self.dialect = 'postgresql+pg8000'
         self.__HOST = None
         self.__PASSWORD = None
         self.__USER = None
@@ -18,21 +20,28 @@ class DatabaseConnector:
         
         return output
     
-    def init_db_engine(self, filename:str):
+    def init_db_engine(self, filename:str, dialect=None):
+        ''''
+        Method initates database and returns an engine object
+        '''
+        
         output = self.read_db_creds(filename)
 
+        self.__HOST = output['RDS_HOST']
+        self.__PASSWORD = output['RDS_PASSWORD']
+        self.__USER = output['RDS_USER']
+        self.__DATABASE = output['RDS_DATABASE']
+        self.__PORT = output['RDS_PORT']
+
         try:
-            self.__HOST = output['RDS_HOST']
-            self.__PASSWORD = output['RDS_PASSWORD']
-            self.__USER = output['RDS_USER']
-            self.__DATABASE = output['RDS_DATABASE']
-            self.__PORT = output['RDS_PORT']
-
-            print('Data successfully read!')
-        except:
-            pass
-
-        conn = create_engine(f'postgresql+pg8000://{self.__USER}:{self.__PASSWORD}@{self.__HOST}:{self.__PORT}/{self.__DATABASE}')
+            # if statement that verifies driver dialect. Default driver dialect set to postgres
+            if dialect == None:
+                conn = create_engine(f'{self.dialect}://{self.__USER}:{self.__PASSWORD}@{self.__HOST}:{self.__PORT}/{self.__DATABASE}')
+            else:
+                conn = create_engine(f'{dialect}://{self.__USER}:{self.__PASSWORD}@{self.__HOST}:{self.__PORT}/{self.__DATABASE}')
+        except SQLAlchemyError as e:
+            error = str(e.__dict__['orig'])
+            return error
         
         return conn
     
@@ -51,16 +60,18 @@ class DatabaseConnector:
                         print(f'Table name: {table_name}')
         except:
             print('Error! Cannot connect to database')
+            raise
 
     def upload_to_db(self, dataframe, tablename:str):
         '''
         upload into the local database. Raise error if 'dataframe' object is not a pandas dataframe
         '''
-        local_conn = self.init_db_engine('config/mysql_creds')
-
+        local_conn = self.init_db_engine('config/mysql_creds', 'mysql+pymysql')
+        
+        # verify file type, then send data to tablename of choice
         if isinstance(dataframe, pd.DataFrame):
             with local_conn.connect() as con:
-                dataframe.to_sql('dim_users', con)
+                dataframe.to_sql(tablename, con)
         else:
             raise TypeError
 
